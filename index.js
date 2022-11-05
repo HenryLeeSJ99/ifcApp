3cc99cbd6bc0e0f2429fdacc90cadbc6da8c723f
Henry Lee <henrylee040799@gmail.com>
2022-11-05 22:38:45 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(req, res){
+  console.log("Server running on port 3000");
+});
